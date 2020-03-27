49488f29cd2d3e6c71dadb1d9385eab03b9e73cd
zulicek <zuliceklora@gmail.com>
2020-03-28 00:45:23 +0100

    added accordion

--- sofa-app/src/components/Cocktail/Cocktail.jsx
+++ sofa-app/src/components/Cocktail/Cocktail.jsx
@@ -1,16 +1,50 @@
 import React from "react";
+import "./Cocktail.scss";
 
 export class Cocktail extends React.Component {
-  render() {
-    const { name, category, alchocolic, glass, instructions, image } = this.props;
+    constructor(props) {
+        super(props);
 
-    return <li>
-        <p>{ name }</p>
-        <p>{ category }</p>
-        <p>{ alchocolic }</p>
-        <p>{ glass }</p>
-        <p>{ instructions }</p>
-        <img src={ image }/>
-    </li>;
+        this.state = {
+            opened: false
+        };
+    }
+
+    toggleAccordion = () => {
+        this.setState({ opened: !this.state.opened})
+    }
+    
+  render() {
+    const {
+      name,
+      category,
+      alchocolic,
+      glass,
+      instructions,
+      image
+    } = this.props;
+
+    return (
+      <li onClick={this.toggleAccordion.bind(this)}>
+        <div className={`accordion${this.state.opened ? " opened" : ""}`}>
+            <div className="accordion-item">
+                <div
+                    className="image-wrapper"
+                    style={{ backgroundImage: `url(${image})` }}
+                    ></div>
+                <h2 className="title">{name}</h2>
+                <div className="icon">
+                    <i className="fa fa-angle-down" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div className="accordion-content">
+                <p>{category}</p>
+                <p>{alchocolic}</p>
+                <p>{glass}</p>
+                <p>{instructions}</p>
+            </div>
+        </div>
+      </li>
+    );
   }
 }
