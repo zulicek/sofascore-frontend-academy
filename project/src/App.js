7c08bba39bee345471455bed3bc171d6585a14b6
zulicek <zuliceklora@gmail.com>
2020-04-13 10:34:39 +0200

    homework6

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
 import { Homework6 } from './modules/Homework6';
+import { RegisterForm } from './modules/Forms/RegisterForm/RegisterForm';
 
 export function App() {
   return (
